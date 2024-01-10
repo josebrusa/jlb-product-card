2dfd3007de76f5820b088bfe4384dcea8f041b84
josebrusa <josebrusa@outlook.com>
2024-01-10 14:58:24 +0100

    version 0.0.1 lista

--- tsdx.config.js
+++ tsdx.config.js
@@ -1,17 +1,14 @@
 const postcss = require('rollup-plugin-postcss');
 const images = require('@rollup/plugin-image');
+const replace = require('@rollup/plugin-replace');
 module.exports = {
   rollup(config, options) {
     config.plugins = [
       postcss({ modules: true }),
       images({ incude: ['**/*.png', '**/*.jpg'] }),
       ...config.plugins,
-      replace({
-        preventAssignment: true,
-        'process.browser': true,
-        'process.env.NODE_ENV': JSON.stringify(mode),
-      }),
     ];
+    options.plugins = [replace({ preventAssignment: true })];
     return config;
   },
 };
